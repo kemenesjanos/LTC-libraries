42bff161f2d2c428bc01b4a614df5beccd7b8509
kemenesjanos <70114756+kemenesjanos@users.noreply.github.com>
2021-01-28 20:15:39 +0100

    Finish SensorsController

--- src/Board.ino
+++ src/Board.ino
@@ -1,13 +1,16 @@
 #include <Arduino.h>
 #include "LedController/LedControllerLibrary.h"
+#include "SensorsController/SensorsControllerLibrary.h"
 
 LedControllerLib alma;
+SensorsControllerLib korte;
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
 }
 
 void loop() {
-  alma.setColor(White);
-  alma.setColor(true, true, false);
+  Serial.println(korte.getSensorValue(2));
+  delay(500);
 }
--- src/SensorsController/SensorsControllerLibrary.cpp
+++ src/SensorsController/SensorsControllerLibrary.cpp
@@ -7,19 +7,19 @@ int SensorsControllerLib::getSensorValue(int serialNumber){
     switch (serialNumber)
     {
     case 0:
-        _sr.set(0, HIGH);
-        _sr.set(1, LOW);
-        _sr.set(2, LOW);
-        break;
-    case 1:
-        _sr.set(0, LOW);
         _sr.set(1, HIGH);
         _sr.set(2, LOW);
+        _sr.set(3, LOW);
         break;
-    case 2:
-        _sr.set(0, LOW);
+    case 1:
         _sr.set(1, LOW);
         _sr.set(2, HIGH);
+        _sr.set(3, LOW);
+        break;
+    case 2:
+        _sr.set(1, LOW);
+        _sr.set(2, LOW);
+        _sr.set(3, HIGH);
         break;
     default:
         return -1;
