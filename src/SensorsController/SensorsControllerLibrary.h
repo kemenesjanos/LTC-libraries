36a94a4075ad8b996cfab69f70a9899777738b2f
kemenesjanos <70114756+kemenesjanos@users.noreply.github.com>
2021-01-28 18:03:21 +0100

    Add shiftregister handler to sensors controller.

--- src/SensorsController/SensorsControllerLibrary.h
+++ src/SensorsController/SensorsControllerLibrary.h
@@ -7,6 +7,8 @@
   #include "WProgram.h"
 #endif
 
+#include <ShiftRegister74HC595.h>
+
 class SensorsControllerLib{
   public:
     SensorsControllerLib();
@@ -16,6 +18,7 @@ class SensorsControllerLib{
     const uint8_t _serialDataPin = 9; // DS
     const uint8_t _clockPin = 10; // SHCP
     const uint8_t _latchPin = 8; // STCP
+    ShiftRegister74HC595<2> _sr = ShiftRegister74HC595<2>(_serialDataPin, _clockPin, _latchPin);
 };
 
 #endif
