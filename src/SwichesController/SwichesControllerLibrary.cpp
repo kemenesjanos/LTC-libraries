135ab79c6448ec837dc2cd015be0e0df6284e4c1
kemenesjanos <70114756+kemenesjanos@users.noreply.github.com>
2021-01-29 17:35:09 +0100

    Set up the frame of SwichesController.

---
+++ src/SwichesController/SwichesControllerLibrary.cpp
@@ -0,0 +1,9 @@
+#include "SwichesControllerLibrary.h"
+
+SwitchesControllerLib::SwitchesControllerLib(){
+  
+}
+
+void SwitchesControllerLib::setSwitch(int serial_number, uint8_t val){
+
+}
---
+++ src/SwichesController/SwichesControllerLibrary.h
@@ -0,0 +1,23 @@
+#ifndef bc
+#define bc
+
+#if (ARDUINO >= 100)
+  #include "Arduino.h"
+#else
+  #include "WProgram.h"
+#endif
+
+#include <ShiftRegister74HC595.h>
+
+class SwitchesControllerLib{
+  public:
+    SwitchesControllerLib();
+    void setSwitch(int serial_number, uint8_t val);
+  private:
+    const uint8_t _serialDataPin = 9; // DS
+    const uint8_t _clockPin = 10; // SHCP
+    const uint8_t _latchPin = 8; // STCP
+    ShiftRegister74HC595<2> _sr = ShiftRegister74HC595<2>(_serialDataPin, _clockPin, _latchPin);
+};
+
+#endif
