d4314003adc3604edf995f5bfcc785a6a8c7228b
kemenesjanos <70114756+kemenesjanos@users.noreply.github.com>
2021-02-16 19:19:54 +0100

    Create Stepper Motor Controller Lib skeleton.

---
+++ src/StepperMotorController/MotorControllerLibrary.h
@@ -0,0 +1,28 @@
+#ifndef sc
+#define sc
+
+#if (ARDUINO >= 100)
+  #include "Arduino.h"
+#else
+  #include "WProgram.h"
+#endif
+
+#include <ShiftRegister74HC595.h>
+
+class MotorControllerLib{
+  public:
+    MotorControllerLib();
+    void turnRight(int degree, int speed);
+    void turnLeft(int degree, int speed);
+  private:
+    const uint8_t _in1_Sh = 4;
+    const uint8_t _in1_Sh = 5;
+    const uint8_t _in1_Sh = 6;
+    const uint8_t _in1_Sh = 7;
+    const uint8_t _serialDataPin = 9; // DS
+    const uint8_t _clockPin = 10; // SHCP
+    const uint8_t _latchPin = 8; // STCP
+    ShiftRegister74HC595<2> _sr = ShiftRegister74HC595<2>(_serialDataPin, _clockPin, _latchPin);
+};
+
+#endif
